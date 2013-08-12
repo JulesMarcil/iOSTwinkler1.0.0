874189cd0746373074130310a5babb78ffd5c47e
ArnaudDri <arnaud.drizard@gmail.com>
2013-08-12 13:15:02 +0200

    welcome page

---
+++ TabBarSlider/TabBarSlider/welcomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  welcomeViewController.h
+//  TabBarSlider
+//
+//  Created by Arnaud Drizard on 12/08/13.
+//  Copyright (c) 2013 Arnaud Drizard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface welcomeViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *registerButton;
+@property (weak, nonatomic) IBOutlet UIButton *signinButton;
+@property (weak, nonatomic) IBOutlet UIImageView *bckgdImage;
+
+@end
---
+++ TabBarSlider/TabBarSlider/welcomeViewController.m
@@ -0,0 +1,55 @@
+//
+//  welcomeViewController.m
+//  TabBarSlider
+//
+//  Created by Arnaud Drizard on 12/08/13.
+//  Copyright (c) 2013 Arnaud Drizard. All rights reserved.
+//
+
+#import "welcomeViewController.h"
+
+@interface welcomeViewController ()
+
+@end
+
+@implementation welcomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [[self navigationController] setNavigationBarHidden:YES animated:YES];
+    
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGFloat screenHeight = screenRect.size.height;
+    
+    CGRect frame= [self.registerButton frame];
+    [self.registerButton setFrame:CGRectMake(15,
+                                       screenHeight-frame.size.height-30,
+                                       frame.size.width,
+                                       frame.size.height)];
+    
+    frame= [self.signinButton frame];
+    [self.signinButton setFrame:CGRectMake(160,
+                                             screenHeight-frame.size.height-30,
+                                             frame.size.width,
+                                             frame.size.height)];
+    frame= [self.bckgdImage frame];
+    [self.bckgdImage setFrame:screenRect];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
