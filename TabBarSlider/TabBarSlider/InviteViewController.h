89767d134c411b0374348117d5eed47ea2558c2a
JulesMarcil <jules.marcilhacy@gmail.com>
2013-08-26 17:19:35 +0200

    addgroup post

---
+++ TabBarSlider/TabBarSlider/InviteViewController.h
@@ -0,0 +1,13 @@
+//
+//  InviteViewController.h
+//  TabBarSlider
+//
+//  Created by Jules Marcilhacy on 26/08/13.
+//  Copyright (c) 2013 Arnaud Drizard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InviteViewController : UIViewController
+
+@end
