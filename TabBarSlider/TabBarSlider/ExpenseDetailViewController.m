1e0182c36e8dd912d7722273212c01a25de5d877
ArnaudDri <arnaud.drizard@gmail.com>
2013-09-25 10:22:18 +0100

    expensedetail fixed

--- TabBarSlider/TabBarSlider/ExpenseDetailViewController.m
+++ TabBarSlider/TabBarSlider/ExpenseDetailViewController.m
@@ -148,17 +148,18 @@
     self.containerView.backgroundColor=[UIColor clearColor];
     
     
-    UIGraphicsBeginImageContextWithOptions(CGSizeMake([[UIScreen mainScreen] bounds].size.width, [[UIScreen mainScreen] bounds].size.height+40),YES,0.0f);
+    UIGraphicsBeginImageContextWithOptions([[UIScreen mainScreen] bounds].size, NO, 0.0f);
     UIViewController *VC=[self presentingViewController];
-    [VC.view.layer setFrame:CGRectMake(VC.view.layer.frame.origin.x,VC.view.layer.frame.origin.y, VC.view.layer.frame.size.width,  VC.view.layer.frame.size.height)];
+    [VC.view.layer setFrame:VC.view.layer.frame];
     [VC.view.layer renderInContext:UIGraphicsGetCurrentContext()];
     
     UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     
-     viewImage=[self imageByScalingAndCroppingForSize:CGSizeMake(self.view.frame.size.width,[[UIScreen mainScreen] bounds].size.height) : viewImage];
     
     self.view.backgroundColor = [UIColor colorWithPatternImage:viewImage];
+    
+    
     
 }
 
@@ -171,62 +172,6 @@
         if (self.scrollView.contentOffset.y<1) {
             [self dismissViewControllerAnimated:NO completion:nil];
         }
-}
-
-- (UIImage*)imageByScalingAndCroppingForSize:(CGSize)targetSize:(UIImage *)srcimage
-{
-    UIImage *sourceImage = srcimage;
-    UIImage *newImage = nil;
-    CGSize imageSize = sourceImage.size;
-    CGFloat width = imageSize.width;
-    CGFloat height = imageSize.height;
-    CGFloat targetWidth = targetSize.width;
-    CGFloat targetHeight = targetSize.height;
-    CGFloat scaleFactor = 0.0;
-    CGFloat scaledWidth = targetWidth;
-    CGFloat scaledHeight = targetHeight;
-    CGPoint thumbnailPoint = CGPointMake(0.0,0.0);
-    
-    if (CGSizeEqualToSize(imageSize, targetSize) == NO)
-    {
-        CGFloat widthFactor = targetWidth / width;
-        CGFloat heightFactor = targetHeight / height;
-        
-        if (widthFactor > heightFactor)
-            scaleFactor = widthFactor; // scale to fit height
-        else
-            scaleFactor = heightFactor; // scale to fit width
-        scaledWidth  = width * scaleFactor;
-        scaledHeight = height * scaleFactor;
-        
-        // center the image
-        if (widthFactor > heightFactor)
-        {
-            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
-        }
-        else
-            if (widthFactor < heightFactor)
-            {
-                thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
-            }
-    }
-    
-    UIGraphicsBeginImageContext(targetSize); // this will crop
-    
-    CGRect thumbnailRect = CGRectZero;
-    thumbnailRect.origin = thumbnailPoint;
-    thumbnailRect.size.width  = scaledWidth;
-    thumbnailRect.size.height = scaledHeight;
-    
-    [sourceImage drawInRect:thumbnailRect];
-    
-    newImage = UIGraphicsGetImageFromCurrentImageContext();
-    if(newImage == nil)
-        NSLog(@"could not scale image");
-    
-    //pop the context to get back to the default
-    UIGraphicsEndImageContext();
-    return newImage;
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)sender {
