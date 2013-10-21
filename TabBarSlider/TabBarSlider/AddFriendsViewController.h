7e3f8d117dcfbb10eb5b4c653884bfd22e807991
JulesMarcil <jules.marcilhacy@gmail.com>
2013-10-21 11:55:32 +0100

    wip add friends

---
+++ TabBarSlider/TabBarSlider/AddFriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddFriendsViewController.h
+//  Twinkler
+//
+//  Created by Jules Marcilhacy on 21/10/13.
+//  Copyright (c) 2013 Arnaud Drizard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFriendsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ TabBarSlider/TabBarSlider/AddFriendsViewController.m
@@ -0,0 +1,51 @@
+//
+//  AddFriendsViewController.m
+//  Twinkler
+//
+//  Created by Jules Marcilhacy on 21/10/13.
+//  Copyright (c) 2013 Arnaud Drizard. All rights reserved.
+//
+
+#import "AddFriendsViewController.h"
+
+@interface AddFriendsViewController ()
+
+@end
+
+@implementation AddFriendsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+@end
